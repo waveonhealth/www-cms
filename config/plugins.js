f4672045a10cce3c1561dc051ace6767c4b715e0
Tom Brown <tbrown@growthpartner.com>
2025-03-05 06:06:08 -0500

    Updated S3

--- config/plugins.js
+++ config/plugins.js
@@ -8,8 +8,6 @@ module.exports = ({ env }) => ({
            secretAccessKey: env('AWS_ACCESS_SECRET'),
            region: env('AWS_REGION'),
            params: {
-             ACL: env('AWS_ACL', 'public-read'),
-             signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
              Bucket: env('AWS_BUCKET'),
            },
          },
